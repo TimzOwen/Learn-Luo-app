c1406457283f163d6cb4b749ceab0f531911de85
Timz Owen <ktimothy@kabarak.ac.ke>
2019-09-17 13:24:06 -0700

    Update NumbersActivity.java
    updated the Java Luo activty

--- src/main/java/com/example/android/miwok/NumbersActivity.java
+++ src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,57 +1,171 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.example.android.miwok;
+package com.owen.miworkapp;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
+
 
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
+    //declare the media player API file
+    private MediaPlayer mMediaPlayer;
+
+    //create an On Completion Listener to check if the son is done playing;
+    private MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaResources();
+        }
+    };
+
+    // Declare a variable to hold the Audion Manager
+    private AudioManager mAudioManager;
+
+    //declare the audioFocus Listner
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focus)
+        {
+            //check on the focus so as to know where to start from
+            if (focus == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK || focus == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT)
+            {
+                //pause and start from begining
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+            }
+            else if (focus == AudioManager.AUDIOFOCUS_GAIN)
+            {
+                mMediaPlayer.start();
+            }
+            else if (focus == AudioManager.AUDIOFOCUS_LOSS)
+            {
+                releaseMediaResources();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.words_list);
 
-       ArrayList<String> words= new ArrayList<String>();
+        //instanciate the Audio Manger
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
-        words.add("one");
-        words.add("Two");
-        words.add("Three");
-        words.add("Four");
-        words.add("Five");
-        words.add("Six");
-        words.add("Seven");
-        words.add("Eight");
-        words.add("Nine");
-        words.add("Ten");
+      final   ArrayList<Word> words = new ArrayList<>();
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
+//        Word w = new Word("Lutti","One");
+//        words.add(w);
+        words.add(new Word("Ndala", "One",R.drawable.number_one, R.raw.number_one));
+        words.add(new Word("Chipli", "Two",R.drawable.number_two, R.raw.number_two));
+        words.add(new Word("Chitaru", "Three",R.drawable.number_three, R.raw.number_three));
+        words.add(new Word("Chinne", "Four",R.drawable.number_four, R.raw.number_four));
+        words.add(new Word("Charano", "Five",R.drawable.number_five, R.raw.number_five));
+        words.add(new Word("Lutti", "Six",R.drawable.number_six, R.raw.number_six));
+        words.add(new Word("Lutti", "Seven",R.drawable.number_seven, R.raw.number_seven));
+        words.add(new Word("Lutti", "Eight",R.drawable.number_eight, R.raw.number_eight));
+        words.add(new Word("Lutti", "Nine",R.drawable.number_nine, R.raw.number_nine));
+        words.add(new Word("Lutti", "Ten",R.drawable.number_ten, R.raw.number_ten));
+        words.add(new Word("Ndala", "One",R.drawable.number_one, R.raw.number_one));
+        words.add(new Word("Chipli", "Two",R.drawable.number_two, R.raw.number_two));
+        words.add(new Word("Chitaru", "Three",R.drawable.number_three, R.raw.number_three));
+        words.add(new Word("Chinne", "Four",R.drawable.number_four, R.raw.number_four));
+        words.add(new Word("Charano", "Five",R.drawable.number_five, R.raw.number_five));
+        words.add(new Word("Lutti", "Six",R.drawable.number_six, R.raw.number_six));
+        words.add(new Word("Lutti", "Seven",R.drawable.number_seven, R.raw.number_seven));
+        words.add(new Word("Lutti", "Eight",R.drawable.number_eight, R.raw.number_eight));
+        words.add(new Word("Lutti", "Nine",R.drawable.number_nine, R.raw.number_nine));
+        words.add(new Word("Lutti", "Ten",R.drawable.number_ten, R.raw.number_ten));
 
-        ListView listView = (ListView) findViewById(R.id.list_item);
 
 
-        listView.setAdapter(arrayAdapter);
+        WordAdapter  adapter = new WordAdapter(this,  words, R.color.category_numbers);
+
+        ListView listView = findViewById(R.id.listRoot);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                releaseMediaResources();
+
+                Word word = words.get(position);
+
+                //Request audio Focus  and play the music.
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+
+                //check f reques has been granted
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+
+                    mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getmSoundPronunciation());
+
+                    //start playing the media song. No need to prepare as it is automatic
+                    mMediaPlayer.start();
+
+                    mMediaPlayer.setOnCompletionListener(onCompletionListener);
+                }
+            }
+        });
+    }
+
+    //Release the media Resource of onStop Life  Circle
 
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaResources();
+    }
+
+    private void releaseMediaResources()
+    {
+        if (mMediaPlayer != null)
+        {
+            mMediaPlayer.release();
+        }
+        mMediaPlayer = null;
+
+        mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.menu_navigate, menu);
+
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.navigateback:
+                startActivity(new Intent(NumbersActivity.this, MainActivity.class));
+                return true;
+            case R.id.add_language:
+                //cal admin method to add languages to the application.
+                startActivity(new Intent(getApplicationContext(), AdminActivity.class));
+                return true;
+            case R.id.exit_app:
+                //exit the application method
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
